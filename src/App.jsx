3299c438659ccc0ec9d1f9507003ad38626feca1
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-05-17 21:57:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import CheckOut from "./component/CheckOut";
+import OrderHistory from "./component/OrderHistory";
+
+function App() {
+  return (
+    <>
+      <CheckOut />
+      <OrderHistory />
+    </>
+  );
+}
+
+export default App;
